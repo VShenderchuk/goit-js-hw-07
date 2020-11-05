f7ac925997cc71ebb6fc19d589169ff19edaf1f9
VShenderchuk <64258673+VShenderchuk@users.noreply.github.com>
2020-11-05 21:15:51 +0200

    adds ===

--- task-6.js
+++ task-6.js
@@ -1,7 +1,7 @@
 const activeInput = document.querySelector("#validation-input");
 activeInput.addEventListener("blur", inputFocus);
 function inputFocus() {
-  if (activeInput.value.length == activeInput.dataset.length) {
+  if (activeInput.value.length === activeInput.dataset.length) {
     activeInput.classList.add("valid");
     activeInput.classList.remove("invalid");
     console.log("green");
